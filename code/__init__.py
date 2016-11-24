727b28ccb9d6c81eb586a3707ecd26f2abd134e4
RalfMayet <trilly@gmail.com>
2016-11-24 10:45:40 +0800

    new folder structure

--- code/__init__.py
+++
@@ -1,4 +0,0 @@
-from .stacked_autoencoder import StackedAutoEncoder
-from .utils import SummaryWriter
-from .input import OpenCVInputLayer
-from .utils.logger import log
---
+++ destin/nodes/__init__.py
@@ -0,0 +1,2 @@
+from .stacked_autoencoder import StackedAutoEncoder
+from .autoencoder import AutoEncoder
