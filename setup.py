dc611fc71ea2c3c0c7a77f562772273c9e8f48a1
RalfMayet <trilly@gmail.com>
2016-11-28 12:27:55 +0800

    modify setup to include specific package names

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from setuptools import find_packages
 from setuptools import setup
 
 setup(name='destin',
-      version='0.1',
+      version='0.2',
       description='Tensorflow implementation of DeSTIN',
       url='http://github.com/elggem/ae-destin',
       author='Ralf Mayet',
       author_email='mail@elggem.pub',
       license='Unlicense',
-      packages=find_packages(),
+      packages=["destin", "destin.utils", "destin.nodes", "destin.input"],
       install_requires=["colorlog"],
       zip_safe=False)
