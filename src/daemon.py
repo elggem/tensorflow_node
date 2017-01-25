55c42aa1466c8678ffaad06c5f646dfe67711bb2
RalfMayet <trilly@gmail.com>
2017-01-25 14:03:46 +0800

    input layer working

--- src/daemon.py
+++ src/daemon.py
@@ -10,26 +10,43 @@ from destin import AutoEncoderNode
 from destin import SummaryWriter
 from destin import ROSInputLayer
 
-rospy.init_node('destin', anonymous=False, log_level=rospy.INFO)
 
-rospy.loginfo("ROS NODE LAUNCHING")
-rospy.loginfo("recording summaries to " + SummaryWriter().get_summary_folder())
+with tf.Session() as sess:
+    rospy.init_node('destin', anonymous=False, log_level=rospy.INFO)
+    
+    rospy.loginfo("ROS NODE LAUNCHING")
+    rospy.loginfo("recording summaries to " + SummaryWriter().get_summary_folder())
+    
+    ae = AutoEncoderNode(
+        session=sess,
+        name="ae",
+        hidden_dim=40
+    )
 
-inputlayer = ROSInputLayer(output_size=(28, 28), batch_size=250)
+    inputlayer = ROSInputLayer(output_size=(28, 28), batch_size=250)
+
+    ae.register_tensor(inputlayer.get_tensor_for_region([0, 14, 14, 14]))
+
+    ae.initialize_graph()
+
+    # initialize summary writer with graph
+    SummaryWriter().writer.add_graph(sess.graph)
+    merged_summary_op = tf.merge_all_summaries()
+
+    iteration = 0
+    
+    def feed_callback(feed_dict):
+        global iteration
+        iteration += 1
 
 
-def feed_callback(feed_dict):
-    global iteration
-    iteration += 1
-
-    for _ in xrange(50):
-        sess.run(merged_train_ops, feed_dict=feed_dict)
-        sess.run(ae_top.train_op, feed_dict=feed_dict)
-
-    summary_str = merged_summary_op.eval(feed_dict=feed_dict)
-    SummaryWriter().writer.add_summary(summary_str, iteration)
-    SummaryWriter().writer.flush()
-
-inputlayer.feed_topic(feed_callback, "/videofile/image_raw")
-
-rospy.spin()
+        for _ in xrange(50):
+            sess.run(ae.train_op, feed_dict=feed_dict)
+    
+        summary_str = merged_summary_op.eval(feed_dict=feed_dict, session=sess)
+        SummaryWriter().writer.add_summary(summary_str, iteration)
+        SummaryWriter().writer.flush()
+    
+    inputlayer.feed_topic(feed_callback, "/videofile/image_raw")
+    
+    rospy.spin()
--- src/destin/input/ros.py
+++ src/destin/input/ros.py
@@ -3,8 +3,8 @@
 import rospy
 import numpy as np
 import os.path
-import cv2
-
+#import cv2
+from skimage import transform
 
 from destin.input import InputLayer
 from sensor_msgs.msg import Image
@@ -23,18 +23,19 @@ class ROSInputLayer(InputLayer):
         def callback(ros_data):
             # use grayscale image
             np_arr = np.fromstring(ros_data.data, np.uint8).reshape(ros_data.width, ros_data.height, 3)
-            image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-            #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            print np_arr
+            channels = np_arr.swapaxes(0,2)
+            gray = (channels[0] + channels[1] + channels[2]) / 3
+            resized = transform.resize(gray, [self.output_size[0], self.output_size[1]]).reshape([self.output_size[0], self.output_size[1], 1])
 
-            self.batch.append(image.reshape([self.output_size[0], self.output_size[1], 1]))
-            
+            self.batch.append(resized)
+
             # batch is full
             if len(self.batch) >= self.batch_size:
                 feed_dict = {self.name + '/input:0': np.array(self.batch)}
-                feed_callback(feed_dict)
                 self.batch = []
-                rospy.logdebug("📸 Evaluated frame %d" % framecount)
+                
+                feed_callback(feed_dict)
+                rospy.logwarn("📸 Evaluated batch")
 
         ## ROS subscribe...
         rospy.logwarn("subscribing")
