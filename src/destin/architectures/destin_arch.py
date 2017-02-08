0294f0d1e29c6ff972996fd290ff0b9a6c3bcf77
RalfMayet <trilly@gmail.com>
2017-02-08 11:18:17 +0300

    todos for daemon.py

--- src/destin/architectures/destin_arch.py
+++ src/destin/architectures/destin_arch.py
@@ -9,5 +9,21 @@ import tensorflow as tf
 class DestinArchitecture(NetworkArchitecture):
 
     def __init__(self, inputlayer, receptive_field=[14,14], stride=[14,14], node_params={type:"AutoEncoderNode"}):
+        """
+         Todo here:
+
+         1) create nodes according to destin layout
+            wire tensors up to each other
+         2) 
+
+         - sess.run will evaluate only the needed computations, so there should be no
+           redudant calcalations if we run all output_tensors, get the values, and run the training ops!
+            http://stackoverflow.com/questions/34010987/does-tensorflow-rerun-for-each-eval-call
+            
+
+         - Autoencoder nodes need to publish their outputs themselves,
+           after they have been evaluated.
+
+        """
         pass
 
