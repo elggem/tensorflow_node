e4a57c1ce48c94626722e90c01af2e1f23ad6fc6
Ralf Mayet <trilly@gmail.com>
2017-03-21 17:11:03 +0800

    compatability with TF 1.0

--- src/tensorflow_node/nodes/regularized_gan.py
+++ src/tensorflow_node/nodes/regularized_gan.py
@@ -62,15 +62,15 @@ class RegularizedGANNode(object):
 
     def initialize_graph(self):
         # store all variables, so that we can later determinate what new variables there are
-        temp = set(tf.all_variables())
+        temp = set(tf.global_variables())
 
         with tf.name_scope(self.scope):
             with tf.variable_scope(self.name):
                 # concatenate input tensors
-                input_concat = tf.concat(1, self.input_tensors)
+                input_concat = tf.concat(axis=1, values=self.input_tensors)
                 input_dim = input_concat.get_shape()[1].value
                 image_shape = (int(np.sqrt(input_dim)), int(np.sqrt(input_dim)), 1)
-                batch_size = input_concat.get_shape()[0].value 
+                batch_size = input_concat.get_shape()[0].value
 
                 self.model = model = RegularizedGAN(
                     output_dist=MeanBernoulli(input_dim),
@@ -88,10 +88,10 @@ class RegularizedGANNode(object):
                     generator_learning_rate=self.generator_learning_rate,
                     discriminator_learning_rate=self.discriminator_learning_rate,
                 )
-        
+
                 algo.input_tensor = input_concat
                 algo.init_opt()
-        
+
                 # set output tensors
                 self.discriminator = model.discriminate(input_concat)
 
@@ -99,14 +99,14 @@ class RegularizedGANNode(object):
                 self.train_op = algo.generator_trainer
 
                 ## initialize new variables
-                self.session.run(tf.initialize_variables(set(tf.all_variables()) - temp))
-        
+                self.session.run(tf.variables_initializer(set(tf.global_variables()) - temp))
+
     def visualize_all_factors(self):
         if self.output_tensor is None:
             self.initialize_graph()
         print self.name + " outputting latent variable visualization to TB..."
         self.algo.visualize_all_factors()
-        
+
     # I/O
     def register_tensor(self, new_tensor):
         # TODO: check if graph is initialized and modify for new input_dim
